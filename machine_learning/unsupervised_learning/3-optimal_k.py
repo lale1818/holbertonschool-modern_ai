35e472ba978f5af48e4053afc4cbf0f64438e70a
lale1818 <13583@holbertonstudents.com>
2026-06-13 19:43:27 +0000

    Deploy task 3: optimal_k implemented successfully

--- machine_learning/unsupervised_learning/3-optimal_k.py
+++ machine_learning/unsupervised_learning/3-optimal_k.py
@@ -1,25 +1,51 @@
+
 #!/usr/bin/env python3
+
 """
+
 Module to find the optimal number of clusters for K-Means
+
 """
+
 from sklearn import metrics
+
 K_Means = __import__('2-k_means').K_Means
 
 
+
+
+
 def optimal_k(X, max_clusters, random_state):
+
     """
+
     Evaluates K-Means clustering quality using silhouette scores and inertia
+
     """
+
     ks = []
+
     inertia_values = []
+
     silhouette_values = []
 
+
+
     for k in range(2, max_clusters + 1):
+
         model = K_Means(X, n_clusters=k, random_state=random_state)
+
         ks.append(k)
+
         inertia_values.append(model.inertia_)
+
         
+
         score = metrics.silhouette_score(X, model.labels_)
+
         silhouette_values.append(score)
 
+
+
     return ks, inertia_values, silhouette_values
+
