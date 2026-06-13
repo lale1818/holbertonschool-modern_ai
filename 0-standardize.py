bb97b9bc9a1e0c006d1ae8709e1794bec75b7fdd
lale1818 <13583@holbertonstudents.com>
2026-06-13 19:22:05 +0000

    Add Task 0: Feature Standardization using StandardScaler

--- 0-standardize.py
+++ 0-standardize.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
-"""Defines a function to standardize tabular data using Scikit-learn"""
+"""
+This module contains a function to standardize tabular data using scikit-learn.
+"""
 from sklearn import preprocessing
 
 
 def Standardize(X):
-    """
-    Standardizes tabular data using Scikit-learn's StandardScaler.
-
-    Args:
-        X (numpy.ndarray): Tabular data of shape (n_samples, n_features)
-
-    Returns:
-        numpy.ndarray: The standardized version of the input data.
-    """
+    """ Standardizes a numpy.ndarray using StandardScaler """
     scaler = preprocessing.StandardScaler()
     return scaler.fit_transform(X)
